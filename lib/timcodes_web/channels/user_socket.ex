ad5a3b61f49c4dc8a4d4f4aca50026109ee90a6c
Timothy Stapleton <retrocede@outlook.com>
2018-01-04 08:41:06 -0700

    some config to get heroku deploys running

--- lib/timcodes_web/channels/user_socket.ex
+++ lib/timcodes_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule TimcodesWeb.UserSocket do
   # channel "room:*", TimcodesWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
